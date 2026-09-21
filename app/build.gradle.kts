5acd9162bb93853745ae92b7d67fe71a9f5489b3
csantiago001 <carlos.santiago2987@gmail.com>
2026-09-21 01:09:06 -0500

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "co.gov.policia.ia"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "co.gov.policia.ia"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+}
